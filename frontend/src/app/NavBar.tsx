d4afb17172c780cb9d171037434f4f32259b2acf
Damian Russak <damian.russak@gmail.com>
2025-07-05 10:15:33 +0200

    Added theme-config

--- frontend/src/app/NavBar.tsx
+++ frontend/src/app/NavBar.tsx
@@ -1,11 +1,17 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { CgGoogleTasks } from 'react-icons/cg'
+import classNames from 'classnames'
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath)
+
   const links = [
-    { label: 'Tasks', href: '/tasks/' },
-    { label: 'Projects', href: '/projects/' },
+    { label: 'Tasks', href: '/tasks' },
+    { label: 'Projects', href: '/projects' },
   ]
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
@@ -13,7 +19,11 @@ const NavBar = () => {
       {links.map((link) => (
         <Link 
           key={link.href} 
-          className='text-zinc-500 hover:text-zinc-800 transform-colors'
+          className={classNames({ 
+            'text-zinc-900 font-bold': currentPath === link.href,
+            'text-zinc-500': currentPath !== link.href,
+            'hover:text-zinc-800 transform-colors': true
+           })}
           href={link.href}>{link.label}</Link>
       ))}
     </nav>
