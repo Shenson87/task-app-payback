3efe2108b4e32c34c5293c5bd7cbb0c681a72aff
Damian Russak <damian.russak@gmail.com>
2025-07-05 20:24:47 +0200

    Refactoring TaskDetails

---
+++ frontend/src/app/tasks/[id]/TaskDetails.tsx
@@ -0,0 +1,22 @@
+import { TaskStatusBadge } from "@/app/components";
+import { Task } from "@/types/task";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import moment from "moment";
+import ReactMarkdown from "react-markdown";
+
+const TaskDetails = ({ task }: { task: Task }) => {
+  return (
+    <>
+      <Heading as="h1">{task.title}</Heading>
+      <Flex gap="2" my="2">
+        <TaskStatusBadge status={task.completed} />
+        <Text>{moment(task.deadline).format("YYYY-MM-DD")}</Text>
+      </Flex>
+      <Card className="prose" mt="4">
+        <ReactMarkdown>{task.description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+};
+
+export default TaskDetails;
--- frontend/src/app/tasks/[id]/page.tsx
+++ frontend/src/app/tasks/[id]/page.tsx
@@ -1,9 +1,8 @@
-import TaskStatusBadge from "@/app/components/TaskStatusBadge";
 import { getTask } from "@/services/tasks";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
-import moment from "moment";
+import { Box, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import ReactMarkdown from "react-markdown";
+import EditTaskButton from "./EditTaskButton";
+import TaskDetails from "./TaskDetails";
 
 interface Props {
   params: { id: string };
@@ -13,16 +12,14 @@ const TaskDetailPage = async ({ params }: Props) => {
   try {
     const task = await getTask(parseInt(params.id));
     return (
-      <div>
-        <Heading as="h1">{task.title}</Heading>
-        <Flex gap="2" my="2">
-          <TaskStatusBadge status={task.completed} />
-          <Text>{moment(task.deadline).format("YYYY-MM-DD")}</Text>
-        </Flex>
-        <Card className="prose" mt="4">
-          <ReactMarkdown>{task.description}</ReactMarkdown>
-        </Card>
-      </div>
+      <Grid columns={{ initial: "1", md: "2" }} gap="5">
+        <Box>
+          <TaskDetails task={task} />
+        </Box>
+        <Box>
+          <EditTaskButton taskId={task.id} />
+        </Box>
+      </Grid>
     );
   } catch (error) {
     console.log(error);
