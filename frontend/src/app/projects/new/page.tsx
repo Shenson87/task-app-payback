1196b13fa75770470ca2a2c32d844bc1a0b6e7ba
Damian Russak <damian.russak@gmail.com>
2025-07-05 12:58:35 +0200

    Building New Project Page

---
+++ frontend/src/app/projects/new/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { createProject } from "@/services/projects";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { projectFormSchema } from "@/app/validationSchemas";
+import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+
+type ProjectForm = z.infer<typeof projectFormSchema>;
+
+const NewProjectPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit, formState: { errors } } = useForm<ProjectForm>({
+    resolver: zodResolver(projectFormSchema),
+  });
+  const [error, setError] = useState('');
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await createProject(data);
+      router.push("/projects");
+    } catch (error) {
+      console.log(error);
+      setError("An error occurred while creating the project.");
+    }
+  });
+
+  return (
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className='mb-5'>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-3" onSubmit={onSubmit}>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <TextField.Root placeholder="Title" {...register("title")} />
+        <ErrorMessage>{errors.deadline?.message}</ErrorMessage>
+        <TextField.Root
+          placeholder="Deadline (YYYY-MM-DD)"
+          {...register("deadline")}
+        />
+        <Button>Submit New Project</Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewProjectPage;
--- frontend/src/app/validationSchemas.ts
+++ frontend/src/app/validationSchemas.ts
@@ -6,3 +6,8 @@ export const taskFormSchema = z.object({
   deadline: z.string().min(1, { message: "Deadline is required" }),
   project_id: z.string().min(1, { message: "Project is required" })
 });
+
+export const projectFormSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }).max(255, { message: "Title must be less than 255 characters" }),
+  deadline: z.string().min(1, { message: "Deadline is required" })
+})
