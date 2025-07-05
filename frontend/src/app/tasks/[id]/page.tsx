20e8179cfbb3fa021aa5104b514979470835f9fe
Damian Russak <damian.russak@gmail.com>
2025-07-05 16:47:34 +0200

    Styling Task Details Page

--- frontend/src/app/tasks/[id]/page.tsx
+++ frontend/src/app/tasks/[id]/page.tsx
@@ -1,28 +1,32 @@
-import { getTask } from '@/services/tasks'
-import moment from 'moment'
-import { notFound } from 'next/navigation'
-import React from 'react'
+import TaskStatusBadge from "@/app/components/TaskStatusBadge";
+import { getTask } from "@/services/tasks";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import moment from "moment";
+import { notFound } from "next/navigation";
 
 interface Props {
-  params: { id: string }
+  params: { id: string };
 }
 
 const TaskDetailPage = async ({ params }: Props) => {
   try {
-    const task = await getTask(parseInt(params.id))
+    const task = await getTask(parseInt(params.id));
     return (
-    <div>
-      <p>{task.title}</p>
-      <p>{task.description}</p>
-      <p>{moment(task.deadline).format("YYYY-MM-DD")}</p>
-      <p>{task.completed ? "Completed" : "Not Completed"}</p>
-    </div>
-  )
-  }
-  catch (error) {
+      <div>
+        <Heading as="h1">{task.title}</Heading>
+        <Flex gap="2" my="2">
+          <TaskStatusBadge status={task.completed} />
+          <Text as="p">{moment(task.deadline).format("YYYY-MM-DD")}</Text>
+        </Flex>
+        <Card>
+          <Text as="p">{task.description}</Text>
+        </Card>
+      </div>
+    );
+  } catch (error) {
     console.log(error);
-    return notFound()
+    return notFound();
   }
-}
+};
 
-export default TaskDetailPage
+export default TaskDetailPage;
